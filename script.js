cf471c672fdfa43bdb67a93ac305cf4f7e20ac2c
kodywilliams94 <kodyw994@gmail.com>
2024-05-01 20:03:39 -0500

    added getHumanChoice

--- script.js
+++ script.js
@@ -1,13 +1,26 @@
+// Get Computer Choice 
 function getComputerChoice() {
     let randomNumber = Math.floor(Math.random() * 3)
     switch (randomNumber) {
         case 0:
-            return 'rock'
+            return 'Rock'
         case 1:
-            return 'paper'
+            return 'Paper'
         case 2:
-        return 'scissors'
+        return 'Scissors'
     }
 }
 console.log(getComputerChoice())
 
+// Get Human Choice 
+function getHumanChoice() {
+    let userPrompt = prompt('Rock, Paper or Scissors?');
+    if(userPrompt === 'rock') {
+        return 'Rock'
+    } else if(userPrompt === 'paper') {
+        return 'Paper'
+    } else if (userPrompt === 'scissors') {
+        return 'Scissors'
+    }
+}
+console.log(getHumanChoice())
