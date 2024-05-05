ce1c570a12b5d138a2464714308a1f3b8db2d6d1
kodywilliams94 <kodyw994@gmail.com>
2024-05-04 23:19:10 -0500

    start and finish

--- script.js
+++ script.js
@@ -1,30 +1,8 @@
-// Players Score 
-let humanScore = 0
-let computerScore = 0
+console.log('Kody Was Here');
 
-// Get Computer Choice 
+const options = ['rock', 'paper', 'scissors'];
+
 function getComputerChoice() {
-    let randomNumber = Math.floor(Math.random() * 3)
-    switch (randomNumber) {
-        case 0:
-            return 'Rock'
-        case 1:
-            return 'Paper'
-        case 2:
-        return 'Scissors'
-    }
+    console.log('Computer')
 }
-console.log(getComputerChoice())
-
-// Get Human Choice 
-function getHumanChoice() {
-    let userPrompt = prompt('Rock, Paper or Scissors?');
-    if(userPrompt === 'rock') {
-        return 'Rock'
-    } else if(userPrompt === 'paper') {
-        return 'Paper'
-    } else if (userPrompt === 'scissors') {
-        return 'Scissors'
-    }
-}
-console.log(getHumanChoice())
+getComputerChoice()
